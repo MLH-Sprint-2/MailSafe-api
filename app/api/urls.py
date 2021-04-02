5bff3248b2d1307b706a991aafc5658674e0bad9
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-02 08:27:17 +0100

    - Add get_domain - Add get_aliases

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from . views import get_aliases
+from . views import get_aliases, get_domains
 urlpatterns = [
     path('aliases', get_aliases, name="get_aliases"),
+    path('domains', get_domains, name="get_domains"),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 import requests
 from django.http import JsonResponse
+import json
 
-# Create your views here.
 
-API_KEY = 1
+# with open('./config.json') as config_file:
+#     config = json.load(config_file)
+DOMAINS = {"domain": ["hash.fyi", "hideaddress.net",
+                      "mailsire.com", "secret.fyi"]
+           }
 
 
 def get_aliases(request):
+    """
+    Returns all the Aliases
+    API_ENDPOINT:api/v1/aliases
+    """
     res = requests.get(
-        f"https://www.thecocktaildb.com/api/json/v1/{ API_KEY }/search.php?s=margarita")
-    return JsonResponse(res.json())
+        "https://api.forwardemail.net/v1/domains/hideaddress.net/aliases", auth=("48f734d41b6ccd608e021093", ''))
+    return JsonResponse(res.json(), safe=False)
+
+
+def get_domains(request):
+    """Return all the domains
+       API_ENDPOINT:api/v1/aliases
+    """
+    return JsonResponse(DOMAINS)
+
+
+def get_alias_filtered(request):
+    """Return Domain filtered by domain
+       API_ENDPOINT:api/v1/aliases/<domains>"""
+    pass
