41751e8f3c48f28d31cde4b8836a15bb78cd1e84
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-04 22:15:48 +0100

    Make Base Alias Table

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Aliases(models.Model):
+    alias = models.CharField(max_length=200)
+    # auto_now_add = True sets the time when we craeted the object
+    date_created = models.DateTimeField(auto_now_add=True)
+    # if user deleted the post gets deleted not the other way around
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, default='working')
