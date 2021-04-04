3dbc18cf512b692176a82493f496c543848cc8f7
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-04 23:24:56 +0100

    Add Aliases Model - created __str__ for Aliases model - Add to admin - add Allowed Host for Admin to Settings

--- app/api/models.py
+++ app/api/models.py
@@ -10,3 +10,11 @@ class Aliases(models.Model):
     # if user deleted the post gets deleted not the other way around
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, default='working')
+
+    def __str__(self):
+        return f"user: {self.user} \n \
+                date_created: {self.date_created} \n \
+                alias: {self.alias} \n \
+                status: {self.status} \n\n"
+# Sample query of getting Alias from username in User Class
+# filter using queryset Aliases.objects.filter(user__username='apratim')
