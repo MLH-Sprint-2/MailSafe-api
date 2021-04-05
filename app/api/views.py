0d80fc4b22e6eb4065c2679dc1aaafb2687d6671
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-05 14:45:07 +0100

    - Remove hardcoding of data

--- app/api/views.py
+++ app/api/views.py
@@ -10,12 +10,12 @@ PASSWORD = os.environ.get('USERNAME')
 DOMAINS_PROVIDED = {"domains": ["swiftmegaminds.tech","hash.fyi", "hideaddress.net",
                                 "mailsire.com", "secret.fyi"]
                     }
-FORWARD_EMAIL_ENDPOINT = "https://api.forwardemail.net/v1/domains/hideaddress.net/aliases"
 
 # Send to environment variable
 
 
-def request_get_util():
+def request_get_util(domain=''):
+    FORWARD_EMAIL_ENDPOINT = f"https://api.forwardemail.net/v1/domains/{domain}/aliases"
     return requests.get(FORWARD_EMAIL_ENDPOINT, auth=(USERNAME, ''))
 
 
@@ -30,6 +30,7 @@ def get_aliases(request):
 
 def get_domains(request):
     """
+    REDUNDANT
     Return all the domains
     API_ENDPOINT:api/v1/aliases/domains
     """
@@ -39,7 +40,7 @@ def get_domains(request):
 def get_alias_filtered(request, DOMAIN):
     """
     Return Domain filtered by domain
-    API_ENDPOINT:api/v1/aliases/<domains>
+    API_ENDPOINT:api/v1/alias/<domains>
     """
     # if(DOMAIN not in DOMAINS_PROVIDED):
     #     response_data = {
@@ -47,7 +48,7 @@ def get_alias_filtered(request, DOMAIN):
     #     }
     #     return Response(data=response_data, status=400, headers=None, content_type="application/json")
 
-    res = request_get_util()
+    res = request_get_util(DOMAIN)
     data = []
     for x in res.json():
         if x["domain"]["name"] == DOMAIN:
