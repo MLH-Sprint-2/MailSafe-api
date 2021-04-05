0bb87694efe105e0a9bfa04a4b2dc71fe8071ff2
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-05 14:27:32 +0100

    - change URLS from review

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
 from . views import get_aliases, get_domains, get_alias_filtered
 urlpatterns = [
-    path('aliases', get_aliases, name="get_aliases"),
-    path('aliases/domains', get_domains, name="get_domains"),
-    path('aliases/domains/<str:DOMAIN>',
+    path('alias', get_aliases, name="get_aliases"),
+    path('domains', get_domains, name="get_domains"),
+    path('alias/<str:DOMAIN>',
          get_alias_filtered, name="get_alias_filtered"),
-
-
 ]
