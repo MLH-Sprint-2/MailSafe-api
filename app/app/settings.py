cf583373ff563c4554d61f2e0c3c271ff72158e6
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-06 05:05:38 +0100

    - Allowed port 3000 and 8000 for react

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:3000"
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+    "http://0.0.0.0:3000",
+
+]
