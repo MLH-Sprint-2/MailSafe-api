84a43b8c36ce88061dfd2bfb0df698ad37450a82
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-06 11:35:00 +0100

    Correct Typo in CORS

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "http://0.0.0.0:8000",
     "http://0.0.0.0:3000",
