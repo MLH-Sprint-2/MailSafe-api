c1f424bd8d4d4c070c16f9cdf6f0268cc2360731
sachin soman <55349036+sachsom95@users.noreply.github.com>
2021-04-06 05:06:33 +0100

    Cors policy (#11)
    * -Add Cors package to requirement.txt
    
    * - Add CORS to Installed apps
    
    * - Add Cors middleware above CommonMiddleware as per Docs
    
    * - Allowed port 3000 and 8000 for react

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'api',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,8 +43,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:3000"
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+    "http://0.0.0.0:3000",
+
+]
