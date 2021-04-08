1d84d93753c8c89e110b9be85305d3d3c0041500
sachsom95 <sachin.soman@ucdconnect.ie>
2021-04-08 23:44:40 +0100

    Add Rest Auth settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,6 +88,17 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
